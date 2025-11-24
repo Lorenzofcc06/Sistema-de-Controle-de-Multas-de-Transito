9953d7239284544c71d2417221bd833c44dbbb32
Lorenzo Facco <lorenzofacco06@gmail.com>
2025-11-24 20:44:59 -0300

    esqueci da package de infração

---
+++ infracao/Infracao.java
@@ -0,0 +1,21 @@
+package infracao;
+
+import veiculo.Veiculo;
+
+public class Infracao {
+	private TipoInfracao tipo;
+	private Veiculo veiculo;
+
+	public Infracao(TipoInfracao tipo, Veiculo veiculo) {
+		this.tipo = tipo;
+		this.veiculo = veiculo;
+	}
+
+	public TipoInfracao getTipo() {
+		return tipo;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+}
---
+++ infracao/TipoInfracao.java
@@ -0,0 +1,24 @@
+package infracao;
+
+public enum TipoInfracao {
+	LEVE(3, 88.30),
+	MEDIA(4, 130.16),
+	GRAVE(5, 195.23)
+	,GRAVISSIMA(7, 293.47);
+
+	private int pontos;
+	private double valor;
+
+	TipoInfracao(int pontos, double valor) {
+		this.pontos = pontos;
+		this.valor = valor;
+	}
+
+	public int getPontos() {
+		return pontos;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+}
