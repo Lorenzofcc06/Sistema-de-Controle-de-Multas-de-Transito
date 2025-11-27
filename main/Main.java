d4a89bba2c8cc504363d2cae014422c9cb217ea0
Lorenzo Facco <lorenzofacco06@gmail.com>
2025-11-27 11:09:38 -0300

    arrumei um bug no main que estava chamando a classe errada

--- main/Main.java
+++ main/Main.java
@@ -12,20 +12,20 @@ import veiculo.Veiculo;
 public class Main {
 	public static void main(String[] args) {
 
-		RegistroInfracao service = new RegistroInfracao(new CalculadoraInfracaoPadrao(),
+		RegistroInfracao registro = new RegistroInfracao(new CalculadoraInfracaoPadrao(),
 				new AvaliadorSituacaoCnhPadrao());
 
-		Motorista motorista = new Motorista("João Silva", "123456789");
-		Veiculo veiculo = new Veiculo("ABC1234", "Corsa", motorista);
+		Motorista motorista = new Motorista("Lorenzo Facco", "123456789");
+		Veiculo veiculo = new Veiculo("LLL4444", "Golf", motorista);
 
 		Infracao infracaoGrave = new Infracao(TipoInfracao.GRAVE, veiculo);
-		service.registrarInfracao(infracaoGrave);
+		registro.registrarInfracao(infracaoGrave);
 
 		Infracao infracaoLeve = new Infracao(TipoInfracao.LEVE, veiculo);
-		service.registrarInfracao(infracaoLeve);
+		registro.registrarInfracao(infracaoLeve);
 
 		int pontos = motorista.getPontosAcumulados();
-		SituacaoCnh situacao = service.consultarSituacao(motorista);
+		SituacaoCnh situacao = registro.consultarSituacao(motorista);
 
 		System.out.println("Pontos do motorista: " + pontos);
 		System.out.println("Situação da CNH: " + situacao);
